afa295944fa9247e7d420937182c91a7b64fd806
Suwath Ch <suwatch@microsoft.com>
2019-12-25 23:28:29 -0800

    add definition

---
+++ Controllers/ProxyController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspnetcore30docker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProxyController : ControllerBase
+    {
+        //// GET: api/Proxy
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        // GET: api/Proxy
+        [HttpGet, HttpPost]
+        public async Task<IActionResult> Invoke()
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.OK);
+            response.Content = new StringContent($"Hello world {DateTime.UtcNow}Z!{Environment.NewLine}", Encoding.UTF8, "text/plain");
+            return await Task.FromResult(new HttpResponseMessageResult(response));
+        }
+
+        // GET: api/Proxy/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Proxy
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        // PUT: api/Proxy/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+
+    public class HttpResponseMessageResult : IActionResult
+    {
+        private readonly HttpResponseMessage _responseMessage;
+
+        public HttpResponseMessageResult(HttpResponseMessage responseMessage)
+        {
+            _responseMessage = responseMessage; // could add throw if null
+        }
+
+        public async Task ExecuteResultAsync(ActionContext context)
+        {
+            var response = context.HttpContext.Response;
+
+
+            if (_responseMessage == null)
+            {
+                var message = "Response message cannot be null";
+
+                throw new InvalidOperationException(message);
+            }
+
+            using (_responseMessage)
+            {
+                response.StatusCode = (int)_responseMessage.StatusCode;
+
+                var responseFeature = context.HttpContext.Features.Get<IHttpResponseFeature>();
+                if (responseFeature != null)
+                {
+                    responseFeature.ReasonPhrase = _responseMessage.ReasonPhrase;
+                }
+
+                var responseHeaders = _responseMessage.Headers;
+
+                // Ignore the Transfer-Encoding header if it is just "chunked".
+                // We let the host decide about whether the response should be chunked or not.
+                if (responseHeaders.TransferEncodingChunked == true &&
+                    responseHeaders.TransferEncoding.Count == 1)
+                {
+                    responseHeaders.TransferEncoding.Clear();
+                }
+
+                foreach (var header in responseHeaders)
+                {
+                    response.Headers.Append(header.Key, header.Value.ToArray());
+                }
+
+                if (_responseMessage.Content != null)
+                {
+                    var contentHeaders = _responseMessage.Content.Headers;
+
+                    // Copy the response content headers only after ensuring they are complete.
+                    // We ask for Content-Length first because HttpContent lazily computes this
+                    // and only afterwards writes the value into the content headers.
+                    var unused = contentHeaders.ContentLength;
+
+                    foreach (var header in contentHeaders)
+                    {
+                        response.Headers.Append(header.Key, header.Value.ToArray());
+                    }
+
+                    await _responseMessage.Content.CopyToAsync(response.Body);
+                }
+            }
+        }
+    }
+}
